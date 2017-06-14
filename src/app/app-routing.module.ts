aef628828192245c997e526594e3ccb990901cbb
maxguzman <maxguzman@GENOS.local>
2017-06-14 17:03:14 -0400

    Routing basic setup.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from "app/signin/signin.component";
+import { HomeComponent } from "app/home/home.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  // pathMatch is just a precaution
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'signin', component: SigninComponent },
+  // if user puts another path manually on url
+  { path: '**', redirectTo: 'home' }
+  
 ];
 
 @NgModule({
@@ -13,3 +18,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+

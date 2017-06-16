555a5685ae486f5c323fc6d9330d76a7841c2269
max.g <maxguzman@users.noreply.github.com>
2017-06-16 11:56:23 -0400

    Receive a route parameter

--- src/app/moench/moench.component.ts
+++ src/app/moench/moench.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
   selector: 'app-moench',
@@ -7,10 +8,16 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class MoenchComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  message = 'Welcome to Moench';
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     document.getElementById('moench-btn').classList.add('active');
+    this.route.params.subscribe( (routeParams: Params) => { 
+        const floor = routeParams['floor'];
+        this.message = `Moench ${floor}`;
+     })
   }
 
   ngOnDestroy() {
